c07836c6af7ed6516fea2d5f3c0200b8f031c769
Habibullah <habibhaseeb716@gmail.com>
2023-07-05 00:01:53 +0500

    Add JSON.js

---
+++ JSON.js
@@ -0,0 +1,26 @@
+var JSON_example =  [{
+    "name": "Ali",
+    "age": 30,
+    "email": "ALi123@gmail.com",
+
+
+    // This lines says the address of that person.
+    "address": {
+      "street": "123 Main Street",
+      "city": "Faisalabad",
+      "state": "NY",
+      "zip": "10001"
+    },
+
+    // This lines of code says the person's phone No:
+    "phoneNumbers": [
+      "+92 1234567891",
+      "43 1234567892"
+    ],
+
+    // This Js code says if the statement is true and skills of that person in a given array.
+    "isEmployed": true,
+    "skills": ["JavaScript", "C#", "HTML", "CSS"]
+  }
+];
+  
